29e2cdfadc22b396fc101f1e1b301770ee12d633
Mike <ZackCipolletta@gmail.com>
2023-02-21 14:41:23 -0800

    create tests for returning the number of pennies and nickels from a given value. Received passing tests.
    Co-authored-by: NicksFed <nick.guzy76@gmail.com>

--- Coins.Tests/ModelsTests/CoinsTests.cs
+++ Coins.Tests/ModelsTests/CoinsTests.cs
@@ -1,6 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Coins.Models;
-
+using System;
 namespace Coins.Tests
 {
   [TestClass]
@@ -23,6 +23,56 @@ namespace Coins.Tests
       //Assert
       Assert.AreEqual(userChange, result);
     }
+
+    [TestMethod]
+    public void GetMoney_DetermineTheNumberOfPenniesToReturn_String()
+    {
+      // Arrange
+      Change penniesToReturn = new Change(.04);
+      // Act
+      string returnCoins = penniesToReturn.GetMoney();
+      double numberOfPennies = penniesToReturn.ChangeValue *100;
+
+      // Assert
+      Assert.AreEqual($"you are getting {numberOfPennies} pennies back", returnCoins);
+    }
+
+    [TestMethod]
+    public void GetMoney_DetermineTheNumberOfNickelsToReturn_String()
+    {
+      // Arrange
+      Change nickelsToReturn = new Change(.07);
+      // Act
+      string returnCoins = nickelsToReturn.GetMoney();
+      double numberOfNickels = Convert.ToInt32(nickelsToReturn.ChangeValue / .05);
+      // int intNickels = Convert.ToInt32(numberOfNickels);
+      // Assert
+      Assert.AreEqual($"you are getting {numberOfNickels} nickels back", returnCoins);
+    }
+
+        [TestMethod]
+    public void GetMoney_DetermineTheNumberOfDimesToReturn_String()
+    {
+      // Arrange
+      Change DimesToReturn = new Change(.12);
+      // Act
+      string returnCoins = DimesToReturn.GetMoney();
+      // Assert
+      Assert.AreEqual("you are getting dimes back", returnCoins);
+    }
+
+            [TestMethod]
+    public void GetMoney_DetermineTheNumberOfQuartersToReturn_String()
+    {
+      // Arrange
+      Change QuartersToReturn = new Change(.27);
+      // Act
+      string returnCoins = QuartersToReturn.GetMoney();
+      // Assert
+      Assert.AreEqual("you are getting quarters back", returnCoins);
+    }
+
+
   }
 }
 
--- Coins/Models/Change.cs
+++ Coins/Models/Change.cs
@@ -1,3 +1,5 @@
+using System;
+
 
 namespace Coins.Models
 {
@@ -8,6 +10,27 @@ namespace Coins.Models
     public Change (double userChange) 
     {
       ChangeValue = userChange;
+    }
+
+    public string GetMoney()
+    {
+      if ( (ChangeValue / .25) >= 1 )
+      {
+        return "you are getting quarters back";
+      }
+      else if ( (ChangeValue / .10) >= 1 )
+      {
+        return "you are getting dimes back";
+      }
+      else if ( (ChangeValue / .05) >= 1 )
+      {
+        return $"you are getting {Convert.ToInt32(ChangeValue / .05)} nickels back";
+      }
+      else
+      {
+
+        return $"you are getting {ChangeValue * 100} pennies back";
+      }
     }
   }
 }
